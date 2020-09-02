a110bcbd55bb182737c6081618a78fd5bb66a8ac
Justinccc11 <47933391+Justinccc11@users.noreply.github.com>
2020-09-02 16:26:25 -0400

    Add files via upload

---
+++ align.py
@@ -0,0 +1,71 @@
+from download import *
+from split_audio_from_vtt import make_corpus
+import os,sys
+import subprocess
+from corpus_to_tuples import get_tuples
+import h5py
+import numpy as np
+import pandas as pd
+
+MFA_ALIGNER = '../montreal-forced-aligner/bin/'
+G2P_MODEL = 'spanish_g2p.zip'
+LANG_MODEL = 'spanish.zip'
+
+def output_h5py(intervals, work_dir, video_id, name):
+    data = {}
+    data[video_id] = {}
+    data[video_id]['intervals'] = np.array([[a[0][0], a[0][1]] for a in intervals])
+    data[video_id]['features'] = np.array([[a[1].encode('utf-8')] for a in intervals],dtype="a32")
+
+    write5Handle=h5py.File(os.path.join(work_dir, video_id + "_" + name + ".h5py"),'w')
+
+    vidHandle=write5Handle.create_group(video_id)
+    vidHandle.create_dataset("features",data=data[video_id]["features"])
+    vidHandle.create_dataset("intervals",data=data[video_id]["intervals"])
+    write5Handle.close()
+
+def align(video_id,lang):
+    os.mkdir(video_id)
+    work_dir = video_id
+    dwnld(video_id, work_dir)
+
+    transcription = os.path.join(work_dir,video_id + '.' + lang + '.vtt')
+    wav = os.path.join(work_dir,video_id + '.wav')
+    unaligned = os.path.join(work_dir,video_id + '_unaligned')
+    make_corpus(wav,transcription,unaligned)
+    corpus = unaligned
+    video_dict = os.path.join(work_dir,video_id + '_dict.txt')
+
+    # generate dictionary
+    subprocess.check_call(MFA_ALIGNER + 'mfa_generate_dictionary ' + G2P_MODEL + ' ' + corpus + ' ' + video_dict,shell=True)
+
+    output_dir = os.path.join(work_dir,video_id + '_aligned')
+    align_call = MFA_ALIGNER + 'mfa_align ' + corpus + ' ' + video_dict + ' ' + LANG_MODEL + ' ' + output_dir
+    subprocess.check_call(align_call,shell=True)
+
+    word_intervals,phone_intervals = get_tuples(output_dir)
+
+    output_h5py(word_intervals, work_dir, video_id, "words")
+    output_h5py(word_intervals, work_dir, video_id, "phones")
+
+    print(word_intervals)
+
+def load_video_ids(file_name, lang):
+    data = pd.read_csv(file_name)
+    L = list(data[lang])
+    return L
+
+
+if __name__ == '__main__':
+    #put the video ids in one column in the csv file
+    file_name = "german.csv" #the name of the csv file containing the video id
+    lang = "german"
+    lang_abbrev = "de"
+    video_ids = load_video_ids(file_name, lang)
+    for video_id in video_ids:
+        try:
+            align(video_id, lang_abbrev)
+        except:
+            print("wrong_lang or align error from mfa")
+            continue
+    print("Finished")
